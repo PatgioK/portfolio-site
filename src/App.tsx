ac0cce0e81b7274a59c8ebf4d01df259a6194af8
pat <patgiok@gmail.com>
2022-08-24 12:16:18 -0700

    first commit, setup components, remove defaults and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import About from './Components/about/About';
+import Footer from './Components/footer/Footer';
+import Header from './Components/header/Header'
+import Nav from './Components/nav/Nav'
+import Portfolio from './Components/portfolio/Portfolio';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <Header />
+    <Nav />
+    <About />
+    <Portfolio />
+    <Footer />
+  </>
   );
 }
 
