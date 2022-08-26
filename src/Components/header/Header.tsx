2ad096fe17230585a9884b4ad3e0e1bf3e92ebee
pat <patgiok@gmail.com>
2022-08-25 20:48:29 -0700

    formatted header + new pfp

--- src/Components/header/Header.tsx
+++ src/Components/header/Header.tsx
@@ -2,20 +2,23 @@ import React from 'react'
 import './header.css'
 import './CTA';
 import CTA from './CTA';
-import pfp from '../../assets/profilepic.jpg'
+import pfp from '../../assets/pfp.png'
+import pffp from '../../assets/profilepic.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
     <header>
-      <div className=' container header_container'>
+      <div className=' container header__container'>
         <h5>Hello I'm</h5>
         <h1>Patgio Kwok</h1>
         <h5 className='text-light'>Fullstack Developer</h5>
-        <CTA />
 
+        <HeaderSocials />
         <div className='me'>
-          <img src={pfp} alt="me" />
+          <img src={pfp} alt="me" width='500' height='500'/>
         </div>
+        <CTA />
       </div>
 
     </header>
