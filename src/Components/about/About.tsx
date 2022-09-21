fd3b51c59ffadaec15b109ad0d5fefa1c8f9f6cc
pat <patgiok@gmail.com>
2022-09-21 13:32:35 -0700

    added more about me, + css

--- src/Components/about/About.tsx
+++ src/Components/about/About.tsx
@@ -16,10 +16,21 @@ const About = (props: aboutProps) => {
 
       <div className='about__content'>
         <p>{props.data?.main.bio}</p>
+        <br />
       </div>
-    </section>
+      <div className="about__technical">
+        <h4>Technical</h4>
+        <p>I'm a recent graduate of BCIT's Computer Systems Technology Program, where I learned industry standards for coding and project management. I am most experienced with Ruby on Rails and React & Redux for quick prototyping and scalability.
+        </p>
+        <br />
+      </div>
+      <div className="about__personal">
+      <h4>Personal</h4>
+      <p> I was born in Hong Kong but moved to Vancouver over 20 years ago. I'm a huge foodie but also enjoy music, gaming and fishing! When at home, I enjoy playing with my cats.</p>
+      </div>
     <br />
       <AboutSocials />
+    </section>
     </div>
   )
 }
